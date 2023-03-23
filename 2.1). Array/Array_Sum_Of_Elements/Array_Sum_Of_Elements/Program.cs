780195b9cec7b646a8b2062ce0c897be22c119dd
Ankit Mehrotra <64391917+ankitmehrotrachemistry@users.noreply.github.com>
2023-03-23 14:58:07 +0530

    Add files via upload

---
+++ 2.1). Array/Array_Sum_Of_Elements/Array_Sum_Of_Elements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Sum_Of_Elements
+{
+    internal class Program
+    {
+        public static int SumOfArray(int[] arr)
+        {
+            int size = arr.Length;
+            int totalSum = 0;
+
+            for(int i = 0; i < size; i++)
+            {
+                totalSum = totalSum + arr[i];
+            }
+            return totalSum;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 12, 10, 2, 17, 6, 40 };
+
+            Console.WriteLine("Sum of Elements is : " +SumOfArray(array));
+        }
+    }
+}
