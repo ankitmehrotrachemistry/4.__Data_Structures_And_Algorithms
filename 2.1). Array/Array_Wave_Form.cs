a10a8d111c16d72bdf0dabf59157320173402897
Ankit Mehrotra <64391917+ankitmehrotrachemistry@users.noreply.github.com>
2023-12-17 17:51:02 +0530

    Create Array_Wave_Form.cs

---
+++ 2.1). Array/Array_Wave_Form.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Array_Wave_Form
+{
+    internal class Program
+    {
+        public static void Swap(int[] arr, int a, int b)
+        {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+        public static void PrintArray(int[] array, int length)
+        {
+            Console.WriteLine("Array Elements in Wave form are : ");
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public static void WaveArray(int[] arr)
+        {
+            int size = arr.Length;
+
+            for (int i = 0; i < size; i += 2)
+            {
+                if ((i - 1) >= 0 && arr[i] > arr[i - 1])
+                {
+                    Swap(arr, i, i - 1);
+                }
+                if ((i + 1) < size && arr[i] > arr[i + 1])
+                {
+                    Swap(arr, i, i + 1);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arrayWave = new int[] { 8, 1, 2, 3, 4, 5, 6, 4, 2 };
+
+            WaveArray(arrayWave);
+
+            PrintArray(arrayWave, arrayWave.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+OUTPUT
+Array Elements in Wave form are :
+1 8 2 4 3 6 4 5 2
+*/
