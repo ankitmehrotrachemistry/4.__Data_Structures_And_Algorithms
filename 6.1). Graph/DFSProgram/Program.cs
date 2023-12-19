3e49945c1f910e85605ad05a39fb52faaff4319a
Ankit Mehrotra <64391917+ankitmehrotrachemistry@users.noreply.github.com>
2023-12-19 23:36:27 +0530

    Folder Change

---
+++ 6.1). Graph/DFSProgram/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DFSProgram
+{
+	public class Graph
+	{
+		private int V;
+
+		private List<int>[] adj;
+		Graph(int v)
+		{
+			V = v;
+			adj = new List<int>[v];
+			for (int i = 0; i < v; ++i)
+				adj[i] = new List<int>();
+		}
+		void addEdge(int v, int w)
+		{
+			adj[v].Add(w);
+		}
+		void DFSUtil(int v, bool[] visited)
+		{
+			visited[v] = true;
+			Console.Write(v + " ");
+
+			foreach (int i in adj[v])
+			{
+				int n = i;
+				if (!visited[n])
+					DFSUtil(n, visited);
+			}
+		}
+
+		void DFS()
+		{
+			bool[] visited = new bool[V];
+
+			for (int i = 0; i < V; ++i)
+				if (visited[i] == false)
+					DFSUtil(i, visited);
+		}
+		public static void Main(String[] args)
+		{
+			Graph g = new Graph(4);
+
+			g.addEdge(0, 1);
+			g.addEdge(0, 2);
+			g.addEdge(1, 2);
+			g.addEdge(2, 0);
+			g.addEdge(2, 3);
+			g.addEdge(3, 3);
+
+			Console.WriteLine("Following is Depth First Traversal");
+
+			g.DFS();
+		}
+	}
+
+}
---
+++ 6.1). Graph/GraphDFSCycleDetect/GraphDFSCycleDetect/DFSCycleDetect.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDFSCycleDetect
+{
+    class DFSCycleDetect
+    {
+        private readonly int V;
+        private readonly List<List<int>> adj;
+
+        public DFSCycleDetect(int V)
+        {
+            this.V = V;
+            adj = new List<List<int>>(V);
+
+            for (int i = 0; i < V; i++)
+                adj.Add(new List<int>());
+        }
+        
+        private bool isCyclicUtil(int i, bool[] visited,
+                                        bool[] recStack)
+        {
+            if (recStack[i])
+                return true;
+
+            if (visited[i])
+                return false;
+
+            visited[i] = true;
+
+            recStack[i] = true;
+            List<int> children = adj[i];
+
+            foreach (int c in children)
+                if (isCyclicUtil(c, visited, recStack))
+                    return true;
+
+            recStack[i] = false;
+
+            return false;
+        }
+
+        private void addEdge(int sou, int dest)
+        {
+            adj[sou].Add(dest);
+        }
+ 
+        private bool isCyclic()
+        { 
+            bool[] visited = new bool[V];
+            bool[] recStack = new bool[V];
+            
+            for (int i = 0; i < V; i++)
+                if (isCyclicUtil(i, visited, recStack))
+                    return true;
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            DFSCycleDetect graph = new DFSCycleDetect(4);
+            graph.addEdge(0, 1);
+            graph.addEdge(0, 2);
+            graph.addEdge(1, 2);
+            graph.addEdge(2, 0);
+            graph.addEdge(2, 3);
+            graph.addEdge(3, 3);
+
+            if (graph.isCyclic())
+                Console.WriteLine("Graph contains cycle");
+            else
+                Console.WriteLine("Graph doesn't "
+                                        + "contain cycle");
+        }
+    }
+}
