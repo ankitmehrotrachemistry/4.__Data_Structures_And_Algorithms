2343c35ee2b8e450f5ba19d69e23abb07b6ca32c
Ankit Mehrotra <64391917+ankitmehrotrachemistry@users.noreply.github.com>
2023-12-29 22:35:40 +0530

    Create Binary_Tree_Total_Nodes.cs

---
+++ 5.1). Binary Tree/Binary_Tree_Total_Nodes.cs
@@ -0,0 +1,92 @@
+using System;
+
+namespace Binary_Tree_Total_Nodes
+{
+    class Program
+    {
+        public class Tree
+        {
+            private Node root;
+            private class Node
+            {
+                internal int value;
+                internal Node leftChild;
+                internal Node rightChild;
+
+                public Node(int value, Node leftChild, Node rightChild)
+                {
+                    this.value = value;
+                    this.leftChild = leftChild;
+                    this.rightChild = rightChild;
+                }
+
+                public Node(int value)
+                {
+                    this.value = value;
+                    leftChild = null;
+                    rightChild = null;
+                }
+            }
+            public Tree()
+            {
+                root = null;
+            }
+            public void CreateCompleteBinaryTree(int[] arr)
+            {
+                root = CreateCompleteBinaryTree(arr, 0);
+            }
+            private Node CreateCompleteBinaryTree(int[] arr, int start)
+            {
+                int size = arr.Length;
+                Node curr = new Node(arr[start]);
+
+                int left = 2 * start + 1;
+                int right = 2 * start + 2;
+
+                if (left < size)
+                {
+                    curr.leftChild = CreateCompleteBinaryTree(arr, left);
+                }
+                if (right < size)
+                {
+                    curr.rightChild = CreateCompleteBinaryTree(arr, right);
+                }
+                return curr;
+            }
+            
+            public int numNodes()
+            {
+                return numNodes(root);
+            }
+            private int numNodes(Node curr) 
+            {
+                if(curr == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return (1 + numNodes(curr.rightChild) + numNodes(curr.leftChild));
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            tree.CreateCompleteBinaryTree(arr);
+
+            Console.WriteLine("Total Number of Nodes in Binary Tree : " + tree.numNodes());
+
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+OUTPUT
+Total Number of Nodes in Binary Tree : 10
+*/
