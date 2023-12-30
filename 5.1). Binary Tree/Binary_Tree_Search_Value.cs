ea6230547e32a18fb3e3f99b32a9cc8f03d566b2
Ankit Mehrotra <64391917+ankitmehrotrachemistry@users.noreply.github.com>
2023-12-30 16:54:51 +0530

    Create Binary_Tree_Search_Value.cs

---
+++ 5.1). Binary Tree/Binary_Tree_Search_Value.cs
@@ -0,0 +1,108 @@
+using System;
+
+namespace Binary_Tree_Search_Value
+{
+    // A full node is a node that has both left and right child.
+    class Program
+    {
+        public class Tree
+        {
+            private Node root;
+            private class Node
+            {
+                internal int value;
+                internal Node leftChild;
+                internal Node rightChild;
+
+                public Node(int value, Node leftChild, Node rightChild)
+                {
+                    this.value = value;
+                    this.leftChild = leftChild;
+                    this.rightChild = rightChild;
+                }
+
+                public Node(int value)
+                {
+                    this.value = value;
+                    leftChild = null;
+                    rightChild = null;
+                }
+            }
+            public Tree()
+            {
+                root = null;
+            }
+            public void CreateCompleteBinaryTree(int[] arr)
+            {
+                root = CreateCompleteBinaryTree(arr, 0);
+            }
+            private Node CreateCompleteBinaryTree(int[] arr, int start)
+            {
+                int size = arr.Length;
+                Node curr = new Node(arr[start]);
+
+                int left = 2 * start + 1;
+                int right = 2 * start + 2;
+
+                if (left < size)
+                {
+                    curr.leftChild = CreateCompleteBinaryTree(arr, left);
+                }
+                if (right < size)
+                {
+                    curr.rightChild = CreateCompleteBinaryTree(arr, right);
+                }
+                return curr;
+            }
+            public bool searchBT(int value)
+            {
+                return searchBTUtil(root, value);
+            }
+            private bool searchBTUtil(Node curr, int value)
+            {
+                bool left, right;
+
+                if(curr == null)
+                {
+                    return false;
+                }
+                if(curr.value == value)
+                {
+                    return true;
+                }
+
+                left = searchBTUtil(curr.leftChild, value);
+                if(left)
+                {
+                    return true;
+                }
+
+                right = searchBTUtil(curr.rightChild, value);
+                if(right)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            tree.CreateCompleteBinaryTree(arr);
+
+            Console.WriteLine("Search Value in Binary Tree : " + tree.searchBT(2));
+            Console.WriteLine("Search Value in Binary Tree : " + tree.searchBT(12));
+
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+OUTPUT
+Search Value in Binary Tree : True
+Search Value in Binary Tree : False
+*/
